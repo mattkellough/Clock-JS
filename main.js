f67024f7888ae4b48e2d3e3a5ddae640a21ab700
Matt Kellough <matthewryankellough@gmail.com>
2016-08-17 14:41:32 -0400

    Clock font styles

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+ctx.strokeStyle = '#28d1fa';
+ctx.lineWidth = 17;
+ctx.lineCap = 'round';
+ctx.shadowBlur = 15;
+ctx.shadowColor = '#28d1fa';
 
 function degToRad(degree) {
   var factor = Math.PI/180;
@@ -16,9 +21,11 @@ function renderTime() {
   var minutes = now.getMinutes();
   var seconds = now.getSeconds();
   var milliseconds = now.getMilliseconds();
+  var newSeconds = seconds+(milliseconds/1000);
 
   // Background
-
+  ctx.fillStyle = '#333';
+  ctx.fillRect(0,0,500,500);
 
   // Hours
   ctx.beginPath();
@@ -33,13 +40,19 @@ function renderTime() {
 
   // Seconds
   ctx.beginPath();
-  ctx.arc(250,250,140,degToRad(270),degToRad((seconds*6)-90));
+  ctx.arc(250,250,140,degToRad(270),degToRad((newSeconds*6)-90));
   ctx.stroke();
 
   // Date
+  ctx.font = "25px Avenir";
+  ctx.fillStyle = '#28d1fa';
+  ctx.fillText(today, 155, 250);
 
 
   // Time
+  ctx.font = "15px Avenir";
+  ctx.fillStyle = '#28d1fa';
+  ctx.fillText(time, 155, 280);
 }
 
-renderTime();
+setInterval(renderTime, 40);
